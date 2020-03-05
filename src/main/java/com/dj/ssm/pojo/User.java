d6e83a1cfcefc97786b3b772969fe0b459a02b21
wjw <w18835460636@163.com>
2020-03-05 17:38:19 +0800

    项目结构初始化

---
+++ src/main/java/com/dj/ssm/pojo/User.java
@@ -0,0 +1,12 @@
+package com.dj.ssm.pojo;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Data
+@Accessors(chain = true)
+public class User {
+
+
+}
---
+++ src/main/java/com/dj/ssm/web/UserController.java
@@ -0,0 +1,14 @@
+package com.dj.ssm.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user/")
+public class UserController {
+
+
+
+
+}
